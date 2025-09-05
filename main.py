d3d21b76dea58c3ad4f3b05c1395187f5817d4fc
11PRIMUS <alokpal2803@gmail.com>
2025-09-05 19:40:49 +0530

    added reminder message for text

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime, timedelta
 import re
+import os
 
+WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
+PHONE_NUMBER_ID= os.getenv("PHONE_NUMBER")
 
 app=FastAPI()
 scheduler = BackgroundScheduler(timezone="Asia/Kolkata")
@@ -25,4 +28,18 @@ def msg(text):
         except Exception:
             return None
     
-        
+def remind_msg(message, to_number):
+    url= f"https://graph.facebook.com/v19.0/{PHONE_NUMBER_ID}/messages"
+    headers={
+        "Authorization": f"Bearer {WHATSAPP_TOKEN}",
+        "Content-Type": "application/json"
+
+    }
+    data={
+        "messaging_product": "whatsapp",
+        "to":to_number,
+        "type": "text",
+        "text": {"body":message}
+    }
+    resp =requests.post(url, headers=headers, json=data)
+
